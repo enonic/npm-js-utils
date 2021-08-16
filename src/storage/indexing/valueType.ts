b3e716205c9966e2a48196e2ea9b088fd0263b8f
Christian Westgaard <cwe@enonic.com>
2021-08-16 11:47:55 +0200

    More value types

--- src/storage/indexing/valueType.ts
+++ src/storage/indexing/valueType.ts
@@ -1,9 +1,12 @@
 export const VALUE_TYPE_ANY:string = 'any';
 export const VALUE_TYPE_BOOLEAN:string = 'boolean';
+export const VALUE_TYPE_DOUBLE:string = 'double';
 export const VALUE_TYPE_GEO_POINT:string = 'geoPoint';
 export const VALUE_TYPE_INSTANT:string = 'instant';
 export const VALUE_TYPE_LOCAL_DATE:string = 'localDate';
 export const VALUE_TYPE_LOCAL_DATE_TIME:string = 'localDateTime';
 export const VALUE_TYPE_LOCAL_TIME:string = 'localTime';
+export const VALUE_TYPE_LONG:string = 'long';
+export const VALUE_TYPE_REFERENCE:string = 'reference';
 export const VALUE_TYPE_SET:string = 'set';
 export const VALUE_TYPE_STRING:string = 'string';
