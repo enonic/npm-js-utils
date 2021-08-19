58c778c0a850943c6a5f95b7b4449e6eab91deb9
Christian Westgaard <cwe@enonic.com>
2021-08-19 09:22:55 +0200

    AGGREGATION constants

---
+++ src/storage/aggregation.ts
@@ -0,0 +1,9 @@
+export const AGGREGATION_COUNT = 'count';
+export const AGGREGATION_DATE_HISTOGRAM = 'dateHistogram';
+export const AGGREGATION_DATE_RANGE = 'dateRange';
+export const AGGREGATION_GEO_DISTANCE = 'geoDistance';
+export const AGGREGATION_MAX = 'max';
+export const AGGREGATION_MIN = 'min';
+export const AGGREGATION_RANGE = 'range';
+export const AGGREGATION_STATS = 'stats';
+export const AGGREGATION_TERMS = 'terms';
