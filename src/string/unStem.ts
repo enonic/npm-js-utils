3a472fbb1ea8d04654c06152ec03792c4061f43a
Christian Westgaard <cwe@enonic.com>
2021-08-24 12:40:06 +0200

    unStemEnglish

--- src/string/unStem.ts
+++ src/string/unStem.ts
@@ -1,13 +1,18 @@
+function unStemEnglish(word :string) :Array<string> {
+	return ['arsen', 'commun','gener',"'s'", "'s",'ied', 's','ies', 'sses','ss', 'us','', 'bb', 'dd','ff', 'gg','bl', 'mm','nn', 'pp','rr', 'at','tt', 'iz','ed', 'eed','ing', 'edly','eedly', 'ingly','anci', 'enci','ogi', 'li','bli', 'abli','alli', 'fulli','lessli', 'ousli','entli', 'aliti','biliti', 'iviti','tional', 'ational','alism', 'ation','ization', 'izer','ator', 'iveness','fulness', 'ousness','icate', 'ative','alize', 'iciti','ical', 'tional','ational', 'ful','ness', 'ic','ance', 'ence','able', 'ible','ate', 'ive','ize', 'iti','al', 'ism','ion', 'er','ous', 'ant','ent', 'ment','ement', 'e','l', 'succeed','proceed', 'exceed','canning', 'inning','earring', 'herring','outing', 'andes','atlas', 'bias','cosmos', 'dying','early', 'gently','howe', 'idly','lying', 'news','only', 'singly','skies', 'skis','sky', 'tying','ugly'].map((post) => `${word}${post}`);
+}
+
 function unStemNorwegian(word :string) :Array<string> {
 	return ['a', 'e', 'ede', 'ande', 'ende', 'ane', 'ene', 'hetene', 'erte',
 		'en', 'heten', 'ar', 'er', 'heter', 's', 'as', 'es', 'edes', 'endes',
 		'enes', 'hetenes', 'ens', 'hetens', 'ers', 'ets', 'et', 'het', 'ert',
 		'ast', 'dt', 'vt', 'leg', 'eleg', 'ig', 'eig', 'lig', 'elig', 'els',
-		'lov', 'elov', 'slov', 'hetslov'].map((post) => `${word}${post}`)
+		'lov', 'elov', 'slov', 'hetslov'].map((post) => `${word}${post}`);
 }
 
 export function unStem(word :string, language: string) :Array<string> {
 	switch (language) {
+		case 'en': return unStemEnglish(word);
 		case 'no': return unStemNorwegian(word);
 		default:
 			throw new Error(`Unsupported lanugage:${language}`);
--- test/string/unStem.test.mjs
+++ test/string/unStem.test.mjs
@@ -5,6 +5,35 @@ import {
 } from '../../dist/esm/index.mjs';
 
 
+describe('unStemEnglish', () => {
+	it('Returns a list of words, with Norwegian endings added', () => {
+		const expected = ['cararsen','carcommun','cargener',"car's'","car's",
+			'caried','cars','caries','carsses','carss','carus','car','carbb',
+			'cardd','carff','cargg','carbl','carmm','carnn','carpp','carrr',
+			'carat','cartt','cariz','cared','careed','caring','caredly',
+			'careedly','caringly','caranci','carenci','carogi','carli','carbli',
+			'carabli','caralli','carfulli','carlessli','carousli','carentli',
+			'caraliti','carbiliti','cariviti','cartional','carational',
+			'caralism','caration','carization','carizer','carator','cariveness',
+			'carfulness','carousness','caricate','carative','caralize',
+			'cariciti','carical','cartional','carational','carful','carness',
+			'caric','carance','carence','carable','carible','carate','carive',
+			'carize','cariti','caral','carism','carion','carer','carous',
+			'carant','carent','carment','carement','care','carl','carsucceed',
+			'carproceed','carexceed','carcanning','carinning','carearring',
+			'carherring','carouting','carandes','caratlas','carbias',
+			'carcosmos','cardying','carearly','cargently','carhowe','caridly',
+			'carlying','carnews','caronly','carsingly','carskies','carskis',
+			'carsky','cartying','carugly'];
+		const actual = unStem('car', 'en');
+		//console.log(toStr(actual));
+		deepStrictEqual(
+			expected,
+			actual
+		)
+	});
+});
+
 describe('unStemNorwegian', () => {
 	it('Returns a list of words, with Norwegian endings added', () => {
 		const expected = ['bila','bile','bilede','bilande','bilende','bilane',
