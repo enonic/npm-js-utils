a77876cd3a933b739bd545ffe33cc1ce9ff7a283
Christian Westgaard <cwe@enonic.com>
2021-09-21 10:41:03 +0200

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const COLON_SIGN = ':'; // Not good in repo names
+export const DOT_SIGN = '.';
+export const ELLIPSIS = '…';
+
+export const RESPONSE_TYPE_JSON = 'text/json;charset=utf-8';
+export const RESPONSE_TYPE_HTML = 'text/html;charset=utf-8';
--- src/index.ts
+++ src/index.ts
@@ -125,9 +125,10 @@ export {
 	toStr
 } from './value';
 
-export const COLON_SIGN = ':'; // Not good in repo names
-export const DOT_SIGN = '.';
-export const ELLIPSIS = '…';
-
-export const RESPONSE_TYPE_JSON = 'text/json;charset=utf-8';
-export const RESPONSE_TYPE_HTML = 'text/html;charset=utf-8';
+export {
+	COLON_SIGN,
+	DOT_SIGN,
+	ELLIPSIS,
+	RESPONSE_TYPE_JSON,
+	RESPONSE_TYPE_HTML
+} from './constants';
