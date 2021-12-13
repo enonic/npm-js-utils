e65ebd0f8312794d81f4549ce6c2eef163dc3a22
Christian Westgaard <cwe@enonic.com>
2021-12-13 09:12:56 +0100

    Fix errors from compilerOption importsNotUsedAsValues: 'error'

--- src/storage/indexing/updateIndexConfigs.ts
+++ src/storage/indexing/updateIndexConfigs.ts
@@ -1,4 +1,4 @@
-import {
+import type {
 	IndexConfigEntry,
 	IndexConfigTemplates
 } from 'enonic-types/node';
--- src/storage/query/addQueryFilter.ts
+++ src/storage/query/addQueryFilter.ts
@@ -1,4 +1,4 @@
-import {
+import type {
 	BasicFilters,
 	BooleanFilter,
 	ExistsFilter,
