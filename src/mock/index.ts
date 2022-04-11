645c730f413b564e7c8ad8b8d26dcedb8d072db2
Christian Westgaard <comlock@gmail.com>
2022-04-11 09:03:38 +0200

    Array deconstruction requires tslib

--- src/mock/index.ts
+++
@@ -1,1 +0,0 @@
-export {JavaBridge} from './JavaBridge'
--- src/value/isGeoPointArray.ts
+++ src/value/isGeoPointArray.ts
@@ -1,4 +1,4 @@
-import type {GeoPointArray} from '../mock/value';
+import type {GeoPointArray} from '../index.d';
 
 import {isNumber} from './isNumber';
 
@@ -9,7 +9,10 @@ export function isGeoPointArray(v :GeoPointArray | unknown) :v is GeoPointArray
 		|| v.length !== 2 // Even if the two first items are valid, if there are more items, it's not a GeoPointArray
 	) { return false; }
 
-	const [lat, lon] :number[] = v;
+	//const [lat, lon] :number[] = v; // error TS2354: This syntax requires an imported helper but module 'tslib' cannot be found.
+	const lat = v[0];
+	const lon = v[1];
+
 	if (!isNumber(lat) && !isNumber(lon)) { return false; }
 
 	if ((lat as number) < -90 || (lat as number) > 90) { return false; }
