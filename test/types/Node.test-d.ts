d530adaaed9e93cd5f12fd57c0f88ee0b6affed1
Christian Westgaard <comlock@gmail.com>
2022-11-25 11:06:18 +0100

    Some Node type tests

---
+++ test/types/Node.test-d.ts
@@ -0,0 +1,161 @@
+import type {
+	CommonNodeProperties,
+	CreateNodeParams,
+	Node,
+	// NodeConfigEntry,
+	// NodeIndexConfigParams,
+	NodeIndexConfigTemplates,
+	NodePropertiesOnCreate,
+	NodePropertiesOnModify,
+	NodePropertiesOnRead,
+	ModifyNode,
+	ModifyNodeParams,
+	// RepoConnection,
+} from '/lib/xp/node';
+
+
+import {
+	expectAssignable,
+	// expectError,
+	expectNotAssignable,
+	// expectType,
+	// printType,
+} from 'tsd';
+
+const commonNodeProperties = {
+	_childOrder: 'whatever',
+	_inheritsPermissions: true,
+	_name: 'whatever',
+	_nodeType: 'whatever',
+	_path: 'whatever',
+	_permissions: [],
+	_state: 'whatever',
+	_ts: 'whatever',
+	_versionKey: 'whatever',
+};
+// printType(commonNodeProperties);
+expectAssignable<CommonNodeProperties>(commonNodeProperties);
+
+const nodeConfigEntry = {
+	decideByType: true,
+	enabled: true,
+	nGram: true,
+	fulltext: true,
+	includeInAllText: true,
+	path: true,
+	indexValueProcessors: ['whatever'],
+	languages: ['whatever'],
+}
+
+const indexConfigWithTemplates = {
+	analyzer: 'whatever',
+	default: 'byType' as NodeIndexConfigTemplates, // TODO: Why do I have to type cast here?
+	configs: [{
+		path: 'whatever',
+		config: nodeConfigEntry
+	}]
+}
+
+const indexConfigWithoutTemplates = {
+	analyzer: 'whatever',
+	default: nodeConfigEntry,
+	configs: [{
+		path: 'whatever',
+		config: nodeConfigEntry
+	}]
+}
+
+const createNodeProperties = {
+	...commonNodeProperties,
+	_indexConfig: indexConfigWithTemplates,
+	_parentPath: 'whatever',
+}
+
+const modifyNodeProperties = {
+	...commonNodeProperties,
+	_id: 'whatever',
+	_indexConfig: indexConfigWithTemplates,
+}
+
+const readNodeProperties = {
+	...commonNodeProperties,
+	_id: 'whatever',
+	_indexConfig: indexConfigWithoutTemplates,
+}
+
+expectAssignable<NodePropertiesOnCreate>(createNodeProperties);
+expectAssignable<CreateNodeParams>(createNodeProperties);
+expectNotAssignable<NodePropertiesOnModify>(createNodeProperties);
+expectNotAssignable<NodePropertiesOnRead>(createNodeProperties);
+
+expectAssignable<NodePropertiesOnModify>(modifyNodeProperties);
+expectAssignable<NodePropertiesOnCreate>(modifyNodeProperties); // because _parentPath is optional
+expectAssignable<CreateNodeParams>(modifyNodeProperties); // because _parentPath is optional
+expectNotAssignable<NodePropertiesOnRead>(modifyNodeProperties);
+
+expectAssignable<NodePropertiesOnRead>(readNodeProperties);
+expectAssignable<NodePropertiesOnCreate>(readNodeProperties); // because _parentPath is optional
+expectAssignable<CreateNodeParams>(readNodeProperties); // because _parentPath is optional
+expectAssignable<NodePropertiesOnModify>(readNodeProperties); // because NodeConfigEntry is allowed inside NodeIndexConfigParams
+
+type MyData = {
+	myString: string
+}
+
+const createMyNode = {
+	...createNodeProperties,
+ 	myString: 'whatever'
+}
+const createMyNodeWithoutMyString = {
+	...createNodeProperties,
+}
+const getMyNode = {
+	...readNodeProperties,
+ 	myString: 'whatever'
+}
+const getMyNodeWithoutMyString = {
+	...readNodeProperties,
+}
+const modifyMyNode = {
+	...modifyNodeProperties,
+ 	myString: 'whatever'
+}
+const modifyMyNodeWithoutMystring = {
+	...modifyNodeProperties,
+}
+
+// expectAssignable<Node<MyData>>(createMyNode);
+expectAssignable<CreateNodeParams<MyData>>(createMyNode);
+expectNotAssignable<CreateNodeParams<MyData>>(createMyNodeWithoutMyString);
+
+expectAssignable<Node<MyData>>(getMyNode);
+expectNotAssignable<Node<MyData>>(getMyNodeWithoutMyString);
+
+expectAssignable<ModifyNode<MyData>>(modifyMyNode);
+expectNotAssignable<ModifyNode<MyData>>(modifyMyNodeWithoutMystring);
+
+expectNotAssignable<Node<MyData>>(modifyMyNode);
+expectAssignable<ModifyNodeParams<MyData>>({
+	key: 'whatever',
+	editor: () => {
+		return modifyMyNode;
+	}
+});
+expectNotAssignable<ModifyNodeParams<MyData>>({
+	key: 'whatever',
+	editor: () => {
+		return modifyMyNodeWithoutMystring;
+	}
+});
+expectAssignable<ModifyNodeParams<MyData>>({ // because NodeConfigEntry is allowed inside NodeIndexConfigParams
+	key: 'whatever',
+	editor: () => {
+		return getMyNode;
+	}
+});
+expectNotAssignable<ModifyNodeParams<MyData>>({
+	key: 'whatever',
+	editor: () => {
+		return createMyNode;
+	}
+});
