e1a828c968f07043f2c177d8178b7fe5173fd5eb
Christian Westgaard <comlock@gmail.com>
2022-11-25 15:24:41 +0100

    remove console.debug

--- src/storage/query/addQueryFilter.ts
+++ src/storage/query/addQueryFilter.ts
@@ -38,14 +38,12 @@ export function addQueryFilter({
 	const returnedFilters: Filter|Filter[] = incomingFilters
 		? JSON.parse(JSON.stringify(incomingFilters)) // dereference
 		: {} ;
-	console.debug('incomingFilters',JSON.stringify(returnedFilters, null, 4));
 	if (Array.isArray(returnedFilters)) {
 		returnedFilters.push(addQueryFilter({
 			clause,
 			filter,
 			// filters: {}
 		}) as Filter);
-		console.debug('returnedFiltersArray',JSON.stringify(returnedFilters, null, 4));
 		return returnedFilters;
 	}
 
@@ -55,25 +53,19 @@ export function addQueryFilter({
 		&& !isHasValueFilter(returnedFilters)
 		&& !isIdsFilter(returnedFilters)
 	) {
-		console.debug('incomingFilters is not one of exists,notExits,hasValue,ids', JSON.stringify(returnedFilters, null, 4));
 		if (!returnedFilters.boolean) {
 			returnedFilters.boolean = {} as BooleanFilter['boolean'];
 		}
 	}
 
 	if (isBooleanFilter(returnedFilters)) {
-		console.debug('isBooleanFilter === true', JSON.stringify(returnedFilters, null, 4));
 		if (!returnedFilters.boolean[clause]) {
-			console.debug(`incomingFilters.boolean.${clause} doesn't exist???`, JSON.stringify(returnedFilters.boolean[clause], null, 4));
 			returnedFilters.boolean[clause] = filter;
 		} else if (Array.isArray(returnedFilters.boolean[clause])) {
-			console.debug(`incomingFilters.boolean.${clause} is an array`);
 			(returnedFilters.boolean[clause] as Filter[]).push(filter);
 		} else {
-			console.debug(`incomingFilters.boolean.${clause} is exists but is not an array`);
 			returnedFilters.boolean[clause] = [(returnedFilters.boolean[clause] as Filter),filter];
 		}
 	}
-	console.debug('returnedFilters',JSON.stringify(returnedFilters, null, 4));
 	return returnedFilters;
 }
