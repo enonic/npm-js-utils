96cf3345137c795aaa2913c1bb1b826304131716
Christian Westgaard <comlock@gmail.com>
2022-12-09 15:04:37 +0100

    Add fieldsContainBoost(fields) #12

---
+++ src/storage/query/dsl/fieldsContainBoost.ts
@@ -0,0 +1,31 @@
+import type {
+	FieldObject,
+	Fields,
+} from '../constants';
+
+
+import {isObject} from '../../../value/isObject';
+import {isString} from '../../../value/isString';
+import {includes as stringIncludes} from '../../../string/includes';
+
+
+export function fieldsContainBoost(fields: Fields): boolean {
+	if (Array.isArray(fields)) {
+		return fields.some((field) => fieldsContainBoost(field));
+	} else if (isObject(fields)) {
+		const {boost} = fields as FieldObject;
+		if (
+			boost // not undefined, null, or 0, -0 and so on
+			&& boost !== 1
+		) {
+			return true;
+		}
+	} else if (isString(fields)) {
+		if (stringIncludes(fields, '^')) {
+			return true;
+		}
+	} else {
+		throw new Error(`fieldsContainBoost: fields is not array, object or string!`);
+	}
+	return false;
+}
---
+++ test/storage/query/dsl/fieldsContainBoost.test.ts
@@ -0,0 +1,111 @@
+import type {Fields} from '../../../../src/storage/query/dsl/constants';
+
+import {equal} from 'assert';
+import {fieldsContainBoost} from '../../../../src/storage/query/dsl/fieldsContainBoost';
+
+const TESTS_NEGATIVE = [
+	'_allText',
+	{
+		field: '_allText'
+	},
+	{
+		field: '_allText',
+		boost: -0
+	},
+	{
+		field: '_allText',
+		boost: 0
+	},
+	{
+		field: '_allText',
+		boost: 1
+	},
+	// {
+	// 	field: '_allText',
+	// 	boost: 1.0 // just becomes 1
+	// }
+	['_allText'],
+	[{
+		field: '_allText',
+		boost: 0
+	}],
+	[
+		'_allText',
+		{
+			field: '_allText',
+			boost: 1
+		}
+	]
+];
+
+const TESTS_POSITIVE = [
+	'_allText^-1.1',
+	'_allText^-1',
+	'_allText^-0.1',
+	'_allText^-0', // "invalid"
+	'_allText^',
+	'_allText^0', // "invalid"
+	'_allText^0.1',
+	'_allText^1', // useless
+	'_allText^1.1',
+	{
+		field: '_allText',
+		boost: -1.1
+	},
+	{
+		field: '_allText',
+		boost: -1
+	},
+	{
+		field: '_allText',
+		boost: -0.1
+	},
+	{
+		field: '_allText',
+		boost: 0.1
+	},
+	{
+		field: '_allText',
+		boost: 1.1
+	},
+	['_allText','_allText^'],
+	[{
+		field: '_allText'
+	},{
+		field: '_allText',
+		boost: 1.1
+	}],
+	[
+		'_allText',
+		{
+			field: '_allText',
+			boost: 1.1
+		}
+	]
+];
+
+
+describe('fieldsContainBoost', () => {
+	describe('--> false', () => {
+		for (let i = 0; i < TESTS_NEGATIVE.length; i++) {
+			const fields = TESTS_NEGATIVE[i] as Fields;
+			it(JSON.stringify(fields), () => {
+				equal(
+					false,
+					fieldsContainBoost(fields)
+				)
+			});
+		}
+	});
+	describe('--> true', () => {
+		for (let i = 0; i < TESTS_POSITIVE.length; i++) {
+			const fields = TESTS_POSITIVE[i] as Fields;
+			it(JSON.stringify(fields), () => {
+				equal(
+					true,
+					fieldsContainBoost(fields)
+				)
+			});
+		}
+	});
+});
