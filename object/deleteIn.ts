6996f0097d5d179f4c1b5ae69a907795d4bdd0de
Christian Westgaard <comlock@gmail.com>
2023-06-06 12:56:25 +0200

    Fix #20 deleteIn support ...paths

--- object/deleteIn.ts
+++ object/deleteIn.ts
@@ -5,18 +5,31 @@ interface NestedRecord {
 // Type alias 'NestedRecord' circularly references itself.ts(2456)
 // type NestedRecord = Record<PropertyKey, string|NestedRecord>;
 
-export default function deleteIn(obj: NestedRecord, path: PropertyKey | PropertyKey[]) {
-	if (!obj || !path) {
+function deleteIn(obj: NestedRecord, ...paths: PropertyKey[]): void
+function deleteIn(obj: NestedRecord, path: PropertyKey): void
+function deleteIn(obj: NestedRecord, paths: PropertyKey[]): void
+function deleteIn(obj: NestedRecord, ...paths: any): void {
+	if (!obj || !paths) {
 		return;
 	}
-	if (typeof path === 'string') {
-		path = path.split('.');
+	const uniformPath: PropertyKey[] = [];
+	for (let i = 0; i < (paths).length; i++) {
+		const path = (paths)[i];
+		if (typeof path === 'string') {
+			path.split('.').forEach(p => uniformPath.push(p));
+		} else if (Array.isArray(path)) {
+			path.forEach(p => uniformPath.push(p));
+		} else { // number | symbol
+			uniformPath.push(path);
+		}
 	}
-	for (let i = 0; i < (path as PropertyKey[]).length - 1; i++) {
-		obj = obj[(path as PropertyKey[])[i] as PropertyKey] as NestedRecord;
+	for (let i = 0; i < uniformPath.length - 1; i++) {
+		obj = obj[uniformPath[i]] as NestedRecord;
 		if (typeof obj === 'undefined') {
 			return;
 		}
 	}
-	delete obj[(path as PropertyKey[]).pop() as PropertyKey];
+	delete obj[uniformPath.pop()];
 }
+
+export default deleteIn;
